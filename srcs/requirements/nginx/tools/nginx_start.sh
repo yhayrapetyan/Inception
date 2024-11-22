5266dbebc4ea27ccc255ae7426a649843c67a3f9
Yurik Hayrapetyan <yuhayrap@c1r2s5.42yerevan.am>
2024-11-22 16:45:52 +0400

    add ssl (not working yet)

--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+#sign certificate
+curl -s https://api.github.com/repos/FiloSottile/mkcert/releases/latest| grep browser_download_url  | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
+mv mkcert-v*-linux-amd64 mkcert
+chmod a+x mkcert
+mkcert $USERNAME.42.fr
+mv $USERNAME.42.fr-key.pem $SSL_KEY
+mv $USERNAME.42.fr.pem $SSL_CRT
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $SSL_KEY -out $SSL_CRT -subj "/C=AM/L=Yerevan/O=42/OU=student/CN=$DOMAIN_NAME"
+
 cd /etc/nginx/sites-available/
 
 if [ -f ./default ]; then
