b2d9667904e32369fd0222752f54a474fbb892cd
Yura <211003yura@gmail.com>
2024-11-27 18:03:28 +0400

    redis alomst ready, need to fix Memory overcommit warning

--- srcs/requirements/wordpress/tools/run_wordpress.sh
+++ srcs/requirements/wordpress/tools/run_wordpress.sh
@@ -4,7 +4,6 @@ if [ ! -e ./wp-config.php ]; then
     rm -rf *
     wp core download --allow-root
 
-
     sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
     sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
     sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
@@ -12,10 +11,10 @@ if [ ! -e ./wp-config.php ]; then
     cp wp-config-sample.php wp-config.php
 
     wp core install --url=$DOMAIN_NAME \
-					--title=$WP_TITLE \
-				 	--admin_user=$WP_ROOT_USER_USERNAME \
-		 			--admin_password=$WP_ROOT_USER_PASSWORD \
-					--admin_email=$WP_ROOT_USER_EMAIL --skip-email --allow-root
+                    --title=$WP_TITLE \
+                    --admin_user=$WP_ROOT_USER_USERNAME \
+                    --admin_password=$WP_ROOT_USER_PASSWORD \
+                    --admin_email=$WP_ROOT_USER_EMAIL --skip-email --allow-root
 
     wp user create $WP_USER_USERNAME $WP_USER_EMAIL --role=$WP_USER_ROLE --user_pass=$WP_USER_PASSWORD --allow-root
 
@@ -24,13 +23,13 @@ if [ ! -e ./wp-config.php ]; then
     chmod -R a+w wp-config.php wp-content
 	  chown -R www-data:www-data wp-config.php wp-content
 
-#    wp plugin install redis-cache --activate --allow-root
-#
-#    wp config set WP_REDIS_HOST $REDIS_HOSTNAME --allow-root
-#    wp config set WP_REDIS_PORT $REDIS_PORT --raw --allow-root
-#    wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-#    wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
-#    wp config set WP_REDIS_CLIENT phpredis --allow-root
+    wp plugin install redis-cache --activate --allow-root
+
+    wp config set WP_REDIS_HOST $REDIS_HOSTNAME --allow-root
+    wp config set WP_REDIS_PORT $REDIS_PORT --raw --allow-root
+    wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+    wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
+    wp config set WP_REDIS_CLIENT phpredis --allow-root
 
 	rm -rf ./wp-config-sample.php
 
@@ -42,8 +41,6 @@ else
     echo "Failed to update WordPress core."
 fi
 
-#sleep 10
-
-#wp redis enable --allow-root
+wp redis enable --allow-root
 
 /usr/sbin/php-fpm8.2 -F
