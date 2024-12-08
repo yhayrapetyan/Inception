43ef76b6c32a4414daa137be1a57c7868407454d
Yura <211003yura@gmail.com>
2024-12-08 18:24:13 +0400

    fix script

--- srcs/requirements/mariadb/tools/run_mariadb.sh
+++ srcs/requirements/mariadb/tools/run_mariadb.sh
@@ -1,4 +1,16 @@
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DB};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
+#!/bin/bash
+
+mkdir -p /var/run/mysqld
+
+service mysql start
+
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DB ;" > db.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%' ;" >> db.sql
+echo "FLUSH PRIVILEGES;" >> db.sql
+
+mysql < db.sql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
